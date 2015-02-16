ae5035360c760bf3446ef4cf386f3da813b7669d
shamidreza <hamidreza.plus@gmail.com>
2015-02-16 14:02:28 -0800

    fixing reading .lab and .wav

--- extract_unit_info.py
+++ extract_unit_info.py
@@ -9,7 +9,7 @@ the unit-selection engine.
 """
 import numpy as np
 from scipy.io.wavfile import read as wread
-import os.path
+from os.path import exists
 
 # change corpus_path to refer to your local version of the repository
 # It assumes there are 'wav', 'lab', and 'pm' directories are available in data
@@ -18,15 +18,15 @@ corpus_path = '/Users/hamid/Code/gitlab/voice-conversion/src/lib/arctic/cmu_us_s
 phoneme_group = {'phone':'class'}
 
 def read_wav(wav_fname):
-    if not os.path.exists(wav_fname):
+    if not exists(wav_fname):
         raise IOError, 'The following file does not exist: ' + wav_fname
     fs, wav = wread(wav_fname)
     return fs, wav
 
 def read_lab(lab_fname):
-    if not os.path.exists(lab_fname):
+    if not exists(lab_fname):
         raise IOError, 'The following file does not exist: ' + lab_fname
-    f=open(lab_name, 'r')
+    f=open(lab_fname, 'r')
     times = [0.0]
     lab = []
     for line in f:
@@ -40,7 +40,7 @@ def read_lab(lab_fname):
     return times, lab
 
 def read_pm(pm_fname):
-    if not os.path.exists(pm_fname):
+    if not exists(pm_fname):
         raise IOError, 'The following file does not exist: ' + pm_fname
     f=open(pm_fname, 'r')
     times = []
@@ -54,11 +54,27 @@ def read_pm(pm_fname):
         times.append(float(pars[0]))
     return times
 
-def extract_info(lab):
+def extract_info(times, labs, wav, fs):
+    diphones = []
+    
     return units
 
 def compute_cepstrum(wav_frame):
     return cep
 
+def get_filenames(file_extension):
+    from glob import iglob
+    fnames = []
+    for fname in iglob(corpus_path+'/'+file_extension+'/*.'+file_extension):
+        fnames.append(fname.split('/')[-1].split('.')[0])
+    return fnames
 
+if __name__ == "__main__":
+    fnames = get_filenames('lab')
+    units = []
+    for fname in fnames:
+        times, labs = read_lab(corpus_path+'/lab/'+fname+'.lab')
+        fs, wav = read_wav(corpus_path+'/wav/'+fname+'.wav')
 
+        cur_units = extract_info(times, labs, wav, fs)
+        pass
